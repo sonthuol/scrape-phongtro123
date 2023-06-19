fae2227cb2d6f474d1c4a83f552002a78c7af037
Mark Son <63505302+sonthuol@users.noreply.github.com>
2023-06-19 23:46:53 +0700

    add data

--- scrape.js
+++ scrape.js
@@ -25,13 +25,191 @@ const scrapeCategories = (browser, url) =>
 
       await page.close();
 
-      resolve();
+      resolve(dataCategory);
     } catch (error) {
       console.log("Loi o scapre categories: " + error);
       rejects(error);
     }
   });
 
+const scapre = (browser, url) =>
+  new Promise(async (resolve, rejects) => {
+    try {
+      let newPage = await browser.newPage();
+      console.log(">> Đã mở tab mới...");
+      newPage.goto(url);
+      console.log(">>> Đã truy cập vào trang " + url);
+      await newPage.waitForSelector("#main");
+      console.log(">>> Đã load xong trang web");
+
+      const scraperData = {};
+
+      // Get header
+      const headerData = await newPage.$eval("header", (el) => {
+        return {
+          title: el.querySelector("h1").innerText,
+          description: el.querySelector("p").innerText,
+        };
+      });
+
+      scraperData.header = headerData;
+
+      // Get link detail items
+      const detailLinks = await newPage.$$eval(
+        "#left-col > section.section-post-listing > ul li",
+        (els) => {
+          detailLinks = els.map((el) => {
+            return el.querySelector(".post-meta > h3 > a").href;
+          });
+          return detailLinks;
+        }
+      );
+
+      // Get detail page
+      const scraperDetail = async (link) =>
+        new Promise(async (resolve, rejects) => {
+          try {
+            let pageDetail = await browser.newPage();
+            await pageDetail.goto(link);
+            console.log(">> truy cap link " + link);
+            await pageDetail.waitForSelector("#main");
+
+            const detailData = {};
+            // Bắt đầu cào
+            // Cào ảnh
+            const images = await pageDetail.$$eval(
+              "#left-col > article > div.post-images > div > div.swiper-wrapper > div.swiper-slide",
+              (els) => {
+                images = els.map((el) => {
+                  if (el.querySelector("img")) {
+                    return el.querySelector("img").src;
+                  } else if (el.querySelector("video")) {
+                    return el.querySelector("video > source").src;
+                  }
+                });
+                return images;
+              }
+            );
+
+            detailData.images = images;
+
+            // Get header detail
+            const header = await pageDetail.$eval(
+              "header.page-header",
+              (el) => {
+                return {
+                  title: el.querySelector("h1 > a").innerText,
+                  star: el
+                    .querySelector("h1 > span")
+                    ?.className.replace(/^\D+/g, ""),
+                  class: {
+                    content: el.querySelector("p").innerText,
+                    classType: el.querySelector("p > a > strong").innerText,
+                  },
+                  address: el.querySelector("address").innerText,
+                  attributes: {
+                    price: el.querySelector(
+                      "div.post-attributes > .price > span"
+                    ).innerText,
+                    acreage: el.querySelector(
+                      "div.post-attributes > .acreage > span"
+                    ).innerText,
+                    published: el.querySelector(
+                      "div.post-attributes > .published > span"
+                    ).innerText,
+                    hashtag: el.querySelector(
+                      "div.post-attributes > .hashtag > span"
+                    ).innerText,
+                  },
+                };
+              }
+            );
+
+            detailData.header = header;
+
+            // Get thong tin mo ta
+            const mainContentHeader = await pageDetail.$eval(
+              "#left-col > article.the-post > section.post-main-content",
+              (el) => el.querySelector("div.section-header > h2").innerText
+            );
+            const mainContentContent = await pageDetail.$$eval(
+              "#left-col > article.the-post > section.post-main-content > div.section-content > p",
+              (els) => (mainContentContent = els.map((el) => el.innerText))
+            );
+
+            detailData.mainContent = {
+              header: mainContentHeader,
+              content: mainContentContent,
+            };
+
+            // Dac diem tin dang
+            const overviewHeader = await pageDetail.$eval(
+              "#left-col > article.the-post > section.post-overview",
+              (el) => el.querySelector("div.section-header > h3").innerText
+            );
+            const overviewContent = await pageDetail.$$eval(
+              "#left-col > article.the-post > section.post-overview > div.section-content > table.table > tbody > tr",
+              (els) =>
+                els.map((el) => ({
+                  name: el.querySelector("td:first-child").innerText,
+                  content: el.querySelector("td:last-child").innerText,
+                }))
+            );
+
+            detailData.overview = {
+              header: overviewHeader,
+              content: overviewContent,
+            };
+
+            // Dac diem lien he
+            const contactHeader = await pageDetail.$eval(
+              "#left-col > article.the-post",
+              (el) =>
+                el.querySelector("section.post-contact")
+                  ? el.querySelector(
+                      "section.post-contact > div.section-header > h3"
+                    ).innerText
+                  : null
+            );
+            const contactContent = await pageDetail.$$eval(
+              "#left-col > article.the-post > section.post-contact > div.section-content > table.table > tbody > tr",
+              (els) =>
+                els.map((el) => ({
+                  name: el.querySelector("td:first-child").innerText,
+                  content: el.querySelector("td:last-child").innerText,
+                }))
+            );
+
+            detailData.contact = {
+              header: contactHeader,
+              content: contactContent,
+            };
+
+            await pageDetail.close();
+            console.log(">> Da dong tab " + link);
+            resolve(detailData);
+          } catch (error) {
+            console.log("Lay data loi " + error);
+
+            rejects(error);
+          }
+        });
+
+      const details = [];
+      for (let link of detailLinks) {
+        const detail = await scraperDetail(link);
+        details.push(detail);
+      }
+
+      scraperData.body = details;
+
+      console.log(">>> Trinh duyet da dong");
+      resolve(scraperData);
+    } catch (error) {
+      rejects(error);
+    }
+  });
 module.exports = {
   scrapeCategories,
+  scapre,
 };
--- scrapeController.js
+++ scrapeController.js
@@ -1,10 +1,62 @@
 const scrapes = require("./scrape");
-
+const fs = require("fs");
 const scrapeController = async (browserInstance) => {
   let url = "https://phongtro123.com/";
+  let indexs = [1, 2, 3, 4];
   try {
     let browser = await browserInstance;
-    let categories = await scrapes.scrapeCategories(browser, url);
+    const categories = await scrapes.scrapeCategories(browser, url);
+    const selectedCategories = categories.filter((category, index) =>
+      indexs.some((i) => i === index)
+    );
+    let result_chothuephongtro = await scrapes.scapre(
+      browser,
+      selectedCategories[0].link
+    );
+    fs.writeFile(
+      "chothuephongtro.json",
+      JSON.stringify(result_chothuephongtro),
+      (err) => {
+        if (err) console.log("Ghi log data that bai " + err);
+        console.log("Ghi log data thanh cong");
+      }
+    );
+    let result_nhachothue = await scrapes.scapre(
+      browser,
+      selectedCategories[1].link
+    );
+    fs.writeFile(
+      "nhachothue.json",
+      JSON.stringify(result_nhachothue),
+      (err) => {
+        if (err) console.log("Ghi log data that bai " + err);
+        console.log("Ghi log data thanh cong");
+      }
+    );
+    let result_chothuecanho = await scrapes.scapre(
+      browser,
+      selectedCategories[2].link
+    );
+    fs.writeFile(
+      "chothuecanho.json",
+      JSON.stringify(result_chothuecanho),
+      (err) => {
+        if (err) console.log("Ghi log data that bai " + err);
+        console.log("Ghi log data thanh cong");
+      }
+    );
+    let result_chothuematbang = await scrapes.scapre(
+      browser,
+      selectedCategories[3].link
+    );
+    fs.writeFile(
+      "chothuematbang.json",
+      JSON.stringify(result_chothuematbang),
+      (err) => {
+        if (err) console.log("Ghi log data that bai " + err);
+        console.log("Ghi log data thanh cong");
+      }
+    );
   } catch (error) {
     console.log("Loi o scrape controller: " + error);
   }
